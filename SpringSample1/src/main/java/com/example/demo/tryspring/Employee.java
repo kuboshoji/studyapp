b9686394bf144352cd77ada6041f3767e917dc7d
kuboshoji <jiangzhijiubao@gmail.com>
2019-06-09 21:52:20 +0900

    init commit

---
+++ SpringSample1/src/main/java/com/example/demo/tryspring/Employee.java
@@ -0,0 +1,13 @@
+package com.example.demo.tryspring;
+
+import lombok.Data;
+
+@Data
+public class Employee{
+	private int employeeId;
+	private String employeeName;
+	private int age;
+	
+		
+	}
+	
